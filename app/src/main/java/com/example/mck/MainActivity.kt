f16df61ea9ff35d6d50b8c7193f89323984816e9
Jess <7yesid@gmail.com>
2023-03-22 05:02:04 -0400

    share sheet

--- app/src/main/java/com/example/mck/MainActivity.kt
+++ app/src/main/java/com/example/mck/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mck
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mck.databinding.ActivityMainBinding
@@ -10,5 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnShare.setOnClickListener { share() }
+    }
+
+    private fun share() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, " Vamos ")
+            type = " text / plain "
+            putExtra(Intent.EXTRA_TITLE, "Quieres un x")
+        }
+        val shareIntent = Intent.createChooser(intent, null)
+        startActivity(shareIntent)
     }
 }
